3ad015eeffd3fd57a40152118606c68a8905c42a
margaretwoodhouse <38228323+margaretwoodhouse@users.noreply.github.com>
2021-03-26 06:44:20 -0500

    Update dagchainer.sh

--- dagchainer.sh
+++ dagchainer.sh
@@ -6,6 +6,7 @@ for sample in *_dagchainer.txt
                 describer=$(echo ${sample} | sed 's/_dagchainer.txt//')
                 echo $describer
 
-perl /DAGCHAINER/run_DAG_chainer.pl -i ${sample} -D 1000000 -g 40000 -A 5
+perl /DAGCHAINER/accessory_scripts/filter_repetitive_matches.pl 1000000 < ${sample} > ${describer}_dagchainer_tandem.filtered
 
-done
+perl /DAGCHAINER/run_DAG_chainer.pl -i ${describer}_dagchainer_tandem.filtered -s -I -D 1000000 -g 40000 -A 5
+       done
